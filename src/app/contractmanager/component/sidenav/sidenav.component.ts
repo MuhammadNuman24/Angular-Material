f9843dc9a48c41d770f4951b30e8266f7858bb46
Muhammad Numan <numanrazzaq24@gmail.com>
2021-09-15 17:20:29 +0500

    Add new layout

--- src/app/contractmanager/component/sidenav/sidenav.component.ts
+++ src/app/contractmanager/component/sidenav/sidenav.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 @Component({
   selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
+  templateUrl:'./sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
+ public isScreenSmall:boolean;
+  constructor(private breakpointobserver:BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpointobserver
+    .observe([`(max-width: &{SMALL_WIDTH_BREAKPOINT}px)`])
+    .subscribe((state:BreakpointState)=>{
+      this.isScreenSmall=state.matches
+    })
   }
 
 }
